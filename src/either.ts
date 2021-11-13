1669091cdfc0f1f7afcd57f53d006ff5d2a433a9
Georgi Spasov <georgi.spasov@quanterall.com>
2021-11-13 22:37:35 +0200

    Add documentation for `Either` utility methods

--- src/either.ts
+++ src/either.ts
@@ -2,14 +2,63 @@ import { Just, Maybe, Nothing } from "./maybe";
 export type Either<L, R> = Left<L, R> | Right<L, R>;
 
 interface EitherUtilities<L, R> {
+  /**
+   * Maps over a `Either` value. If the value is a `Right`, it will apply the given
+   * function to the value. Otherwise, it will return the `Left` that is already there.
+   * @param f Mapping function
+   */
   map<T>(f: (r: R) => T): Either<L, T>;
+  /**
+   * Takes the value of the `Either`, if it is `Right` it will apply the given function
+   * over it, returning a new `Either` of type `<T>`. Otherwise it will return the
+   * `Left` that is already there.
+   * @param f Transformer function
+   */
   bind<E, T>(f: (r: R) => Either<E, T>): Either<E, T>;
+  /**
+   * Takes the value of the `Either`, if it is `Right` it will apply the given function
+   * over it and return the result, if it is `Left` it will return the supplied
+   * default value.
+   * @param f Function
+   * @param onNothing Default value if the value of `Either` is `Left`
+   */
   fold<T>(defaultR: T, f: (r: R) => T): T;
+  /**
+   * Maps over a `Either` value. If the value is a `Right`, it will apply the given
+   * async function to the value. Otherwise, it will return the `Left`
+   * that is already there.
+   * @param f Mapping async function
+   */
   mapAsync<T>(f: (r: R) => Promise<T>): Promise<Either<L, T>>;
+  /**
+   * Takes the value of the `Either`, if it is `Right` it will apply the given async
+   * function over it, returning a new `Either` of type `<U>`.
+   * Otherwise it will return the `Left` that is already there.
+   * @param f Transformer async function
+   */
   bindAsync<E, T>(f: (r: R) => Promise<Either<E, T>>): Promise<Either<E, T>>;
+  /**
+   * Takes the value of the `Either`, if it is `Right` it will apply the given async
+   * function over it, if it is `Left` it will return the supplied default value.
+   * @param f Mapping async function
+   * @param onNothing Default value if the value of `Either` is `Left`
+   */
   foldAsync<T>(defaultR: T, f: (r: R) => Promise<T>): Promise<T>;
+  /**
+   * Executes a callback function if the value of the `Either` is `Right`
+   * @param f Callback function
+   */
   onRight(f: (r: R) => void): Either<L, R>;
+  /**
+   * Executes a callback function if the value of the `Either` is `Left`
+   * @param f Callback function
+   */
   onLeft(f: () => void): Either<L, R>;
+  /**
+   * Transforms `Either` type to `Maybe`. If it's a `Right` it will return
+   * a `Just` with the same value inside. If it's a `Left` it will return
+   * Nothing.
+   */
   toMaybe(): Maybe<R>;
   isRight(): this is Right<L, R>;
   isLeft(): this is Left<L, R>;
@@ -26,7 +75,7 @@ export function Right<L, R>(r: R): Either<L, R> {
     value: r,
     map: (f) => Right(f(r)),
     bind: (f) => f(r),
-    fold: (_default, f) => f(r),
+    fold: (_d, f) => f(r),
     mapAsync: async (f) => Right(await f(r)),
     bindAsync: async (f) => await f(r),
     foldAsync: async (_d, f) => await f(r),
